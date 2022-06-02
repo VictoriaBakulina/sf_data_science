2a9e758e93c1ac62566e4fa76d93e7b7c89c8f15
Victoria <mailofvictoriabakulina@gmail.com>
2022-06-02 23:36:02 +0300

    Final commit

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+    predict_number = 0
+
+    while number != predict_number: # предполагаем рандомное число, пока не угадали
+        count += 1
+        predict_number = np.random.randint(1, 101)
+    return count
+
 def score_game(random_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
